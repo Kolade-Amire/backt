34e22f9cb16a7201783bd84718b0888a37ab26e0
Kolade-Amire <stephamire@gmail.com>
2024-10-03 11:09:13 +0100

    project init

---
+++ src/main/java/com/dev/backt/BacktApplication.java
@@ -0,0 +1,13 @@
+package com.dev.backt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BacktApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BacktApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dev/backt/BacktApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.backt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BacktApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
