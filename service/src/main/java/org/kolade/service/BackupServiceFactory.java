2454e10dba3e922b8a263c2e2839aafd8c781095
Kolade-Amire <stephamire@gmail.com>
2024-10-26 22:34:45 +0100

    backup service factory class added

--- service/src/main/java/org/kolade/service/BackupServiceFactory.java
+++ service/src/main/java/org/kolade/service/BackupServiceFactory.java
@@ -1,9 +1,41 @@
 package org.kolade.service;
 
 import lombok.RequiredArgsConstructor;
+import org.kolade.core.DatabaseType;
+import org.kolade.core.exception.CustomBacktException;
+import org.kolade.core.interfaces.BackupService;
+import org.kolade.mongodb.MongoBackupService;
+import org.kolade.mysql.MySQLBackupService;
+import org.kolade.postgresql.PostgresBackupService;
 import org.springframework.stereotype.Component;
 
+import java.util.EnumMap;
+import java.util.Map;
+
 @Component
-@RequiredArgsConstructor
 public class BackupServiceFactory {
+    private final Map<DatabaseType, BackupService> backupServiceMap;
+
+    public BackupServiceFactory(
+            MySQLBackupService mySQLBackupService,
+            PostgresBackupService postgresBackupService,
+            MongoBackupService mongoBackupService
+    ) {
+
+        this.backupServiceMap = new EnumMap<>(DatabaseType.class);
+        backupServiceMap.put(DatabaseType.MYSQL, mySQLBackupService);
+        backupServiceMap.put(DatabaseType.POSTGRES, postgresBackupService);
+        backupServiceMap.put(DatabaseType.MONGODB, mongoBackupService);
+    }
+
+    public BackupService getBackupService(String databaseType) {
+        DatabaseType type = DatabaseType.getTypeByDisplayName(databaseType);
+        BackupService backupService = backupServiceMap.get(type);
+        if (backupService == null) {
+            throw new CustomBacktException("Unsupported database type: " + databaseType);
+        }
+
+        return backupService;
+    }
+
 }
